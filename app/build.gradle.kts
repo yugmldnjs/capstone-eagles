af9e755390fda452031d5e23bcb2cd4e63187213
yuna <spkwnf081620@naver.com>
2025-09-29 17:46:26 +0900

    프로젝트 gradle 파일 (app)

---
+++ app/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.example.capstone"
+    compileSdk = 36
+viewBinding.isEnabled=true  // 뷰바인딩
+    defaultConfig {
+        applicationId = "com.example.capstone"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // 이거 2개 추가
+    implementation("androidx.core:core-splashscreen:1.0.1")  // 앱 시작 화면
+    implementation("androidx.preference:preference-ktx:1.2.1")  // 안드로이드 설정창
+}
