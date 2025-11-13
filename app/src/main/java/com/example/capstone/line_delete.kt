53bbb8ce617f6914be6d5201d0d141a158f78571
yuna <spkwnf081620@naver.com>
2025-11-13 11:31:17 +0900

    설정 탭에서 뒤로가기 버튼 추가

--- app/src/main/java/com/example/capstone/line_delete.kt
+++ app/src/main/java/com/example/capstone/line_delete.kt
@@ -1,9 +1,11 @@
 package com.example.capstone
 
 import android.content.Context
+import android.content.Intent
 import android.util.AttributeSet
 import androidx.preference.Preference
 import androidx.preference.PreferenceViewHolder
+import com.example.capstone.databinding.SettingTopBinding
 
 // 설정의 프로필 사진 밑에 있는 줄을 없애는 용도
 class line_delete(context: Context, attrs: AttributeSet?) : Preference(context, attrs) {
@@ -11,5 +13,12 @@ class line_delete(context: Context, attrs: AttributeSet?) : Preference(context, 
     override fun onBindViewHolder(holder: PreferenceViewHolder) {
         super.onBindViewHolder(holder)
         holder.isDividerAllowedBelow = false
+
+        // 설정 창에서 뒤로가기 버튼 눌렀을 때 메인 카메라 화면으로 되돌아가게함
+        val binding = SettingTopBinding.bind(holder.itemView)
+        binding.settingBackBtn.setOnClickListener {
+            val intent = Intent(context, MainActivity2::class.java)
+            context.startActivity(intent)
+        }
     }
 }
